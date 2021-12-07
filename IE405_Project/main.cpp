c5e62f2930bea7ba15253eb8bab3af636a9c3963
ellenkirsh <ellenek2@illinois.edu>
2021-12-07 13:04:22 -0600

    Initial Commit

---
+++ IE405_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  IE405_Project
+//
+//  Created by Ellen Kirsh on 12/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
